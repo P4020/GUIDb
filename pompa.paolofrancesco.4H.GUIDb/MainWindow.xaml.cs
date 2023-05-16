71edec52fe45efa2f621c500a1509e21ee82b3d3
Paolo Francesco Pompa <117436985+P4020@users.noreply.github.com>
2023-05-16 15:34:35 +0200

    Add files via upload

---
+++ pompa.paolofrancesco.4H.GUIDb/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SQLite;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pompa.paolofrancesco._4H.GUIDb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+            var tblArtists = cn1.Query<Artist>("select * from artists");
+            Console.WriteLine($"In questa tabella ci sono {tblArtists.Count} record!");
+
+            tblArtists = tblArtists.OrderByDescending(x=>x.Name).ToList();
+            foreach(var artista in tblArtists)
+            {
+                Console.WriteLine($"{artista.Name}");
+            }          
+            
+        }
+    }
+}
